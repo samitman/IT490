c1ef87546f4b4dfbf1d1fa49f96295ba52f370d6
Sam <si237@njit.edu>
2022-02-16 01:40:37 +0000

    updated rmq send file

--- RabbitMQ/send.py
+++ RabbitMQ/send.py
@@ -8,7 +8,7 @@ channel.queue_declare(queue='hello')
 
 channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body='Hello World!')
+                      body='Hello from Sam on RabbitMQ!')
 print(" [x] Sent 'Hello World!'")
 
 connection.close()
