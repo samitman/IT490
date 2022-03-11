5d553b7936c4222a1ae40f1e6890c87603b82497
RyanMarinas <ryanmarinas101@gmail.com>
2022-03-11 17:39:22 -0500

    BE to DB vice versa for RMQ1 and RMQ2

---
+++ backend/dbreceive.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import pika, sys, os
+
+def main():
+    credentials = pika.PlainCredentials(username='test', password='test')
+    connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.192.60', credentials=credentials))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='stock')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+    channel.basic_consume(callback, queue='stock', no_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
