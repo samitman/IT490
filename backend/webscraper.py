0641449b60e3a34b4faea173b0b5b6614c495bd2
RyanMarinas <ryanmarinas101@gmail.com>
2022-03-10 21:30:35 -0500

    Backend yahoo finance to database

--- backend/webscraper.py
+++ backend/webscraper.py
@@ -1,10 +1,25 @@
+#!/usr/bin/env python3
 from bs4 import BeautifulSoup
 import requests
+import pika
 
-
+#webscrape for TSLA price from yahoo finance
 url = 'https://finance.yahoo.com/quote/TSLA/'
 page = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",})
 soup = BeautifulSoup(page.text, 'html.parser')
-price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
-#price = soup.find('fin-streamer', {'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[0].text
-print(price)
+stockpricelist = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
+
+print(stockpricelist)
+
+credentials = pika.PlainCredentials(username='test', password='test')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.192.60', credentials=credentials))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='stock')
+
+channel.basic_publish(exchange='',
+                  routing_key='hello',
+                  body= stockpricelist)
+print(" [x] Sent 'Webscraping data from yahoo finance'")
+connection.close()
