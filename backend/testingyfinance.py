53b94bcc2647611a6961cd5ae867340bce3716f0
RyanMarinas <ryanmarinas101@gmail.com>
2022-03-15 19:24:20 -0400

    Testing yfinance

--- backend/testingyfinance.py
+++ backend/testingyfinance.py
@@ -4,17 +4,17 @@ import yfinance as yf
 
 stocks = ['AAPL', 'MSFT', 'AMD']
 
-def main():
-    for stock in stocks:
-        info = yf.Ticker(stock).info
-        marketprice = info.get('regularMarketPrice')
-        print(stock, marketprice)
+#def main():
+for stock in stocks:
+    info = yf.Ticker(stock).info
+    marketprice = info.get('regularMarketPrice')
+    print(stock, marketprice)
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
 
 credentials = pika.PlainCredentials(username='test', password='test')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.192.61', credentials=credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.192.60', credentials=credentials))
 
 channel = connection.channel()
 
@@ -22,6 +22,6 @@ channel.queue_declare(queue='user')
 
 channel.basic_publish(exchange='',
                   routing_key='user',
-                  body= main)
+                  body= stock)
 print(" [x] Sent 'Webscraping data from yahoo finance'")
 connection.close()
