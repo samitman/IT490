f2d7b2f6319822d205223d68d3ed2b2f10df15df
scronin9031 <spc8@njit.edu>
2022-03-19 21:13:02 -0400

    rpc login test
    testing login via rpc

---
+++ database/rpc_client_logintest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import pika
+import uuid
+
+class RegistrationClient(object):
+
+    def __init__(self):
+        credentials = pika.PlainCredentials(username='test', password='test')
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='192.168.192.61', credentials=credentials))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(queue=self.callback_queue, consumer_callback=self.on_response)
+#            queue=self.callback_queue,
+ #           on_message_callback=self.on_response,
+  #          auto_ack=True)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, userinfo):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='rpc_login',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body= userinfo)
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+
+userregistration = RegistrationClient()
+
+print(" [x] Requesting to login")
+response = userregistration.call('Xx_NOSCOPEKING420_xX,password123')
+print(response)
