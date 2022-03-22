33f4e7ee1d3e63ddfc1e9fc2378e493c5b7ad5d3
samad-njit <sas23@njit.edu>
2022-03-22 17:42:31 -0400

    updating HTML

--- html/index.php
+++ html/index.php
@@ -2,8 +2,9 @@
 <link rel="stylesheet" href="static/css/styles.css">
 <br>
 <div>
-<div>
-	<p><img src="images/walnuts_header.png" alt="Walnuts logo"></p>
+<div style="text-align: center;">
+	<b>Walnuts Investment Platform</b><br>
+	<p><img src="images/walnuts_header.png" alt="Newark Academy logo" width="760", height="383"></p>
 	Please log in or register to continue.
 </div>
 <br>
@@ -18,70 +19,5 @@
 		</div>
 	</form>
 </div>
-
-/*<?php
-if (isset($_POST["login"])) {
-    $email = null;
-    $password = null;
-    if (isset($_POST["email"])) {
-        $email = $_POST["email"];
-    }
-    if (isset($_POST["password"])) {
-        $password = $_POST["password"];
-    }
-    $isValid = true;
-    if (!isset($email) || !isset($password)) {
-        $isValid = false;
-        flash("Email/username or password is missing.");
-    }
-    if ($isValid) {
-        $db = getDB();
-        if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, email, username, password, firstName, lastName from Users WHERE username = :email OR email = :email LIMIT 1");
-
-            $params = array(":email" => $email);
-            $r = $stmt->execute($params);
-            $e = $stmt->errorInfo();
-            if ($e[0] != "00000") {
-                flash("Something went wrong, please try logging in again.");
-            }
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($result && isset($result["password"])) {
-                $password_hash_from_db = $result["password"];
-                if (password_verify($password, $password_hash_from_db)) {
-                    $stmt = $db->prepare("SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
-                    $stmt->execute([":user_id" => $result["id"]]);
-                    $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                    unset($result["password"]);//remove password so we don't leak it beyond this page
-                    //let's create a session for our user based on the other data we pulled from the table
-                    $_SESSION["user"] = $result;//we can save the entire result array since we removed password
-                    if ($roles) {
-                        $_SESSION["user"]["roles"] = $roles;
-                    }
-                    else {
-                        $_SESSION["user"]["roles"] = [];
-                    }
-
-	            $_SESSION["user"]["firstName"] = $result["firstName"];
-		    $_SESSION["user"]["lastName"] = $result["lastName"];
-
-                    //on successful login let's serve-side redirect the user to the home page
-                    die(header("Location: home.php"));
-                }
-                else {
-                    flash("You've enetered an invalid password.");
-                }
-            }
-            else {
-                flash("That user does not exist.");
-            }
-        }
-    }
-    else {
-        flash("I'm sorry, something is wrong. Please try again.");
-    }
-}
-?>*/
 
 <?php require(__DIR__ . "/partials/flash.php");
---
+++ html/register.php
@@ -0,0 +1,23 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+
+<br>
+<div style="text-align: center;">
+	<b>Walnuts Investment Platform</b><br>
+        New User Registration<br>
+</div>
+
+<div style="width: 60%; margin: auto;">
+    <form method="POST">
+	<input style="width: 100%" type="text" id="firstName" name="firstName" placeholder="First Name" required/><br><br><br>
+        <input style="width: 100%" type="text" id="lastName" name="lastName" placeholder="Last Name" required/><br><br><br>
+        <input style="width: 100%" type="email" id="email" name="email" placeholder="Email address" required/><br><br><br>
+        <input style="width: 100%" type="text" id="user" name="username" required maxlength="60" placeholder="Desired username"/><br><br><br>
+        <input style="width: 100%" type="password" id="p1" name="password" placeholder="Password" required/><br><br><br>
+        <input style="width: 100%" type="password" id="p2" name="confirm" placeholder="Confirm password" required/><br><br><br>
+	<div style="display: flex;">
+		<a style="text-decoration: none;" href="/index.php" class="submitButton" type="button">Go Back</a>
+        	<input class="submitButton" style="float: right;" type="submit" name="register" value="Register"/><br><br>
+	</div>
+    </form>
+</div>
+<?php require(__DIR__ . "/partials/flash.php");
