6445c092871d43697f05d9933c42593f2b2104b7
RyanMarinas <78096277+RyanMarinas@users.noreply.github.com>
2022-03-25 14:01:25 -0400

    Customizeable portfolios

---
+++ backend/portfolios.py
@@ -0,0 +1,29 @@
+import requests
+import pika
+import yfinance as yf
+
+portfolios = {'etfMeme': ['AMC', 'DOGE-USD', 'GME'], 'etfMedium': ['TSLA','NIO','NVDA']}
+
+for category in portfolios:
+    sum = 0
+    for stock in portfolios[category]:
+        info = yf.Ticker(stock).info
+        marketprice = info.get('regularMarketPrice')
+        sum += marketprice
+        message= stock +","+ str(round(marketprice,2))
+        print(message)
+        message2= category +","+ str(round(sum,2))
+    #print (category + ": " + str(round(sum,2)))
+
+    credentials = pika.PlainCredentials(username='test', password='test')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.192.60', credentials=credentials))
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue='stock')
+
+    channel.basic_publish(exchange='',
+                    routing_key='stock',
+                    body= message + message2)
+    print('Sent', message, message2)
+    connection.close()
