0ada17aa55de5db3e2f9d7a6bcc49e46db3c934d
Sam Itman <si237@njit.edu>
2022-03-30 15:13:24 -0400

    made edits to login test on FE

--- html/logintest.php
+++ html/logintest.php
@@ -1,6 +1,7 @@
 <?php
 $email = "samads@me.com";
 $password = "test";
-$command = "../database/rpc_fe_logintest.py $email, $password";
-shell_exec($command);
+$command = escapeshellcmd('../database/rpc_fe_logintest.py' . $email . $password);
+$output = shell_exec($command);
+echo $output;
 ?>
