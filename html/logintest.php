379b1799185feb83c3102d5b36d5abdf0690420f
FE-Admin <feadmin@walnuts-fe.c.it490-fe.internal>
2022-03-30 19:15:23 +0000

    fixed some errors in logintest

--- html/logintest.php
+++ html/logintest.php
@@ -1,7 +1,7 @@
 <?php
 $email = "samads@me.com";
 $password = "test";
-$command = escapeshellcmd('../database/rpc_fe_logintest.py' . $email . $password);
+$command = escapeshellcmd('../database/rpc_fe_logintest.py' .' '. $email .' ' .$password);
 $output = shell_exec($command);
 echo $output;
 ?>
