98de9ecd238755ca69b63dae903cdc46137ee062
cc862 <cc862@njit.edu>
2022-04-01 18:53:30 -0400

    success with login.py for login success with rpc_client2.py for register

--- html_c/login.php
+++ html_c/login.php
@@ -1,78 +1,43 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 
-<form method="POST">
-    <p>
-        <label for="email">Email / Username:</label>
-        <input type="text" id="email" name="email" required/>
-    </p>
-    <p>   
-	<label for="p1">Password:</label>
-        <input type="password" id="p1" name="password" required/>
-    </p>
-    <p>
-        <input type="submit" name="login" value="Log In"/>
-    </p>
-</form>
 
-<?php
-if (isset($_POST["login"])) {
-    $email = null;
-    $password = null;
-    if (isset($_POST["email"])) {
-        $email = $_POST["email"];
-    }
-    if (isset($_POST["password"])) {
-        $password = $_POST["password"];
-    }
-    $isValid = true;
-    if (!isset($email) || !isset($password)) {
-        $isValid = false;
-        flash("Email/username or password is missing.");
-    }
-    if ($isValid) {
-        $db = getDB();
-        if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE username = :email OR email = :email LIMIT 1");
+<br>
+<div style="text-align: center;">
+	<b>Walnuts Investment Platform</b><br>
+        Login page<br>
+</div>
+<br>
+<div style="width: 60%; margin: auto;">
+    <form method="POST">
+	<!--<input style="width: 100%" type="text" id="firstName" name="firstName" placeholder="First Name" /><br><br><br>
+        <input style="width: 100%" type="text" id="lastName" name="lastName" placeholder="Last Name" /><br><br><br>
+        <input style="width: 100%" type="email" id="email" name="email" placeholder="Email address" /><br><br><br>-->
+        <input style="width: 100%" type="text" id="user" name="username"  maxlength="60" placeholder="Desired username"/><br><br><br>
+        <input style="width: 100%" type="password" id="p1" name="password" placeholder="Password" /><br><br><br>
+       <!-- <input style="width: 100%" type="password" id="p2" name="confirm" placeholder="Confirm password" /><br><br><br>-->
+	<div style="display: flex;">
+		<a style="text-decoration: none;" href="index.php" class="submitButton" type="button">Go11 Back</a>
+        	<input style="float: right;" class="submitButton" type="submit" id="login" name="login" value="login"/><br><br>
+	</div>
+    </form>
+    <?php
+	if(array_key_exists('login', $_POST))
+	{
+		 if (isset($_POST["username"])) 
+		 {
+        		$username = $_POST["username"];}
+        	 if (isset($_POST["password"])) 
+        	 {
+       		 $password = $_POST["password"];}
+		//$result1 = exec("/opt/lampp/htdocs/html/sendc.py 2>&1");
+		//$result2 = exec("/opt/lampp/htdocs/html/rpc_client.py 2>&1");
+		$result3 = exec("python3 login.py $username $password");
+		//echo $result1;
+		//echo $result2;
+		echo $result3;
+		//echo "hello";
+	} ?>
+</div>
 
-            $params = array(":email" => $email);
-            $r = $stmt->execute($params);
-            $e = $stmt->errorInfo();
-            if ($e[0] != "00000") {
-                flash("Something went wrong, please try logging in again.");
-            }
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($result && isset($result["password"])) {
-                $password_hash_from_db = $result["password"];
-                if (password_verify($password, $password_hash_from_db)) {
-                    $stmt = $db->prepare("SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
-                    $stmt->execute([":user_id" => $result["id"]]);
-                    $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                    unset($result["password"]);//remove password so we don't leak it beyond this page
-                    //let's create a session for our user based on the other data we pulled from the table
-                    $_SESSION["user"] = $result;//we can save the entire result array since we removed password
-                    if ($roles) {
-                        $_SESSION["user"]["roles"] = $roles;
-                    }
-                    else {
-                        $_SESSION["user"]["roles"] = [];
-                    }
-                    //on successful login let's serve-side redirect the user to the home page.
-                    flash("You are now logged in.");
-                    die(header("Location: home.php"));
-                }
-                else {
-                    flash("You've enetered an invalid password.");
-                }
-            }
-            else {
-                flash("That user does not exist.");
-            }
-        }
-    }
-    else {
-        flash("I'm sorry, something is wrong. Please try again.");
-    }
-}
-?>
 <?php require(__DIR__ . "/partials/flash.php");
---
+++ html_c/rpc_client2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import pika
+import uuid
+import sys, os
+
+username = sys.argv[1]
+password = sys.argv[2]
+creds = str(username+","+password)
+
+class RegistrationClient(object):
+
+    def __init__(self):
+        credentials = pika.PlainCredentials("test", "test")
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='192.168.192.61', credentials=credentials))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(queue=self.callback_queue, on_message_callback=self.on_response)
+#            queue=self.callback_queue,
+ #           on_message_callback=self.on_response,
+  #          auto_ack=True)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, userinfo):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='rpc_queue',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body= userinfo)
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+
+userregistration = RegistrationClient()
+
+print(" [x] Requesting to register a new user")
+response = userregistration.call(creds)
+Fe_response = response.decode()
+if Fe_response == "1": print("account created")
+else: print("user already exist")
+
+#print(Fe_response)
+
