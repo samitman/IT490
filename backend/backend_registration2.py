4baf7870af07b3602173ba5c424125a8aeab6635
Sam Itman <si237@njit.edu>
2022-04-05 19:09:51 -0400

    trying to pass arguments to python file

--- backend/backend_registration2.py
+++ backend/backend_registration2.py
@@ -3,49 +3,52 @@ import pika
 import uuid
 import sys
 
-username = sys.argv[1]
-password = sys.argv[2]
-creds = str(username)+','+str(password)
+#username = sys.argv[1]
+#password = sys.argv[2]
+#creds = str(username)+','+str(password)
 
-class RegistrationClient(object):
+def main(username,password):
+    creds = str(username)+','+str(password)
 
-    def __init__(self):
-        credentials = pika.PlainCredentials(username='test', password='test')
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='192.168.192.60', credentials=credentials))
+    class RegistrationClient(object):
 
-        self.channel = self.connection.channel()
+        def __init__(self):
+            credentials = pika.PlainCredentials(username='test', password='test')
+            self.connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host='192.168.192.60', credentials=credentials))
 
-        result = self.channel.queue_declare(queue='', exclusive=True)
-        self.callback_queue = result.method.queue
+            self.channel = self.connection.channel()
 
-        self.channel.basic_consume(queue=self.callback_queue, consumer_callback=self.on_response)
-#            queue=self.callback_queue,
- #           on_message_callback=self.on_response,
-  #          auto_ack=True)
+            result = self.channel.queue_declare(queue='', exclusive=True)
+            self.callback_queue = result.method.queue
 
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
+            self.channel.basic_consume(queue=self.callback_queue, consumer_callback=self.on_response)
+    #            queue=self.callback_queue,
+    #           on_message_callback=self.on_response,
+    #          auto_ack=True)
 
-    def call(self, userinfo):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange='',
-            routing_key='rpc_reg_be_db',
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
-            ),
-            body= userinfo)
-        while self.response is None:
-            self.connection.process_data_events()
-        return self.response
+        def on_response(self, ch, method, props, body):
+            if self.corr_id == props.correlation_id:
+                self.response = body
+
+        def call(self, userinfo):
+            self.response = None
+            self.corr_id = str(uuid.uuid4())
+            self.channel.basic_publish(
+                exchange='',
+                routing_key='rpc_reg_be_db',
+                properties=pika.BasicProperties(
+                    reply_to=self.callback_queue,
+                    correlation_id=self.corr_id,
+                ),
+                body= userinfo)
+            while self.response is None:
+                self.connection.process_data_events()
+            return self.response
 
 
-userregistration = RegistrationClient()
+    userregistration = RegistrationClient()
 
-print(" [x] Requesting to login")
-response = userregistration.call(creds)
-print(response)
+    print(" [x] Requesting to login")
+    response = userregistration.call(creds)
+    print(response)
