f72486a31822f4fe6b0dca86fb0a8969cef1bf42
Sam Itman <si237@njit.edu>
2022-04-05 20:27:51 -0400

    changed print statement on bad login

--- html_c/login.py
+++ html_c/login.py
@@ -50,6 +50,6 @@ userregistration = RegistrationClient()
 response = userregistration.call(creds)
 Fe_response = response.decode()
 if Fe_response == "1": print("Success")
-#else: print("Invalid username or password.")
+else: print("")
 
 print(Fe_response)
