907aa4868308a3e66f3f45d64b2c311e6641588d
Sam Itman <si237@njit.edu>
2022-04-13 11:28:22 -0400

    deposit and withdraw forms

--- html_c/transact.php
+++ html_c/transact.php
@@ -16,11 +16,25 @@ else {
 ?>
 </div>
 
-<div>
+<div style="text-align: center;">
 	<t1>Please Pick a Transaction Type:</t1><br>
 	<select id="method" name="method">
 		<option value="" disabled selected>Choose an Option</option>
 		<option value="deposit">Deposit</option>
 		<option value="withdraw">Withdraw</option>
-	</select>
+	</select><br>
+
+	<form id="despositForm" method="POST">
+		<input style="width: 100%" type="number" id="depositAmount" name="depositAmount" placeholder="Amount to Deposit" /><br><br><br>
+	<div style="display: flex;">
+        	<input style="float: right;" class="submitButton" type="submit" id="submitDeposit" name="submitDeposit" value="Deposit"/><br><br>
+	</div>
+    </form>
+
+	<form id="withdrawForm" method="POST">
+		<input style="width: 100%" type="number" id="withdrawAmount" name="withdrawAmount" placeholder="Amount to Withdraw" /><br><br><br>
+	<div style="display: flex;">
+        	<input style="float: right;" class="submitButton" type="submit" id="submitWithdraw" name="submitWithdraw" value="Withdraw"/><br><br>
+	</div>
+    </form>
 </div>
