a97a4c2ace8341e8bece607152936b61eeee4ebf
Sam Itman <si237@njit.edu>
2022-04-13 18:54:53 -0400

    db files for performing deposits and withdrawals

---
+++ database/db_deposit.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import pika, sys, os, mysql.connector
+
+credentials = pika.PlainCredentials(username='test', password='test')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.192.60', credentials=credentials))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='deposit_be_db')
+ 
+
+def dbinsertion(depositDict):
+    mydb = mysql.connector.connect(
+  host="localhost",
+  user="test",
+  password="1234",
+  database='test'
+)
+    msg = ''
+    print(depositDict)
+    cursor = mydb.cursor()
+    select_stmt=('SELECT * FROM accounts WHERE Username = %(Username)s AND Password = %(Password)s')
+    #UPDATE accounts set balance = balance + %(Deposit)s where Username = %(Username)
+    cursor.execute(select_stmt, depositDict)
+    account = cursor.fetchone()
+    if account:
+        print("Account found, depositing cash!")
+        msg = '1'
+        return msg
+    else:
+        print("Deposit failed")
+        msg = '0'
+        return msg
+
+def on_request(ch, method, props, body):
+    print(" [x] Received %r" % body)
+    print(type(body))
+
+    messagestring = body.decode()
+    depositList = messagestring.split(',')
+    username = depositList[0]
+    depositAmount = depositList[1]  
+    print("Split check:" + username +" "+ depositAmount)
+    print(depositList)
+    depositDict =  {"Username": username,"Deposit": depositAmount }
+
+    
+    response = dbinsertion(depositDict)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(consumer_callback=on_request, queue='deposit_be_db')
+
+print(" [x] Awaiting RPC requests")
+print({on_request})
+channel.start_consuming()
---
+++ database/db_withdraw.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import pika, sys, os, mysql.connector
+
+credentials = pika.PlainCredentials(username='test', password='test')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.192.60', credentials=credentials))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='withdraw_be_db')
+ 
+
+def dbinsertion(withdrawDict):
+    mydb = mysql.connector.connect(
+  host="localhost",
+  user="test",
+  password="1234",
+  database='test'
+)
+    msg = ''
+    print(withdrawDict)
+    cursor = mydb.cursor()
+    select_stmt=('SELECT * FROM accounts WHERE Username = %(Username)s AND Password = %(Password)s')
+    #UPDATE accounts set balance = balance - %(Withdraw)s where Username = %(Username)
+    cursor.execute(select_stmt, withdrawDict)
+    account = cursor.fetchone()
+    if account:
+        print("Account found, withdrawing cash!")
+        msg = '1'
+        return msg
+    else:
+        print("Withdraw failed")
+        msg = '0'
+        return msg
+
+def on_request(ch, method, props, body):
+    print(" [x] Received %r" % body)
+    print(type(body))
+
+    messagestring = body.decode()
+    withdrawList = messagestring.split(',')
+    username = withdrawList[0]
+    withdrawAmount = withdrawList[1]  
+    print("Split check:" + username +" "+ withdrawAmount)
+    print(withdrawList)
+    withdrawDict =  {"Username": username,"Withdraw": withdrawAmount }
+
+    
+    response = dbinsertion(withdrawDict)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(consumer_callback=on_request, queue='withdraw_be_db')
+
+print(" [x] Awaiting RPC requests")
+print({on_request})
+channel.start_consuming()
