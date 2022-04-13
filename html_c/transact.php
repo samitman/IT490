d20f59d4906681e1cf3f4621cee10e06e62f6b6e
Sam Itman <si237@njit.edu>
2022-04-13 11:21:17 -0400

    modified dropdown

--- html_c/transact.php
+++ html_c/transact.php
@@ -1,7 +1,7 @@
 <?php require_once(__DIR__ . "/partials/nav.php");?>
 
 <div style="text-align: center;">
-<t>Welcome to the Transaction Center!</t>
+<t>Welcome to the Transaction Center!</t><br>
 <?php
 /* if (isset($_SESSION["user"])) {
 	$username = $_SESSION["user"];
@@ -17,9 +17,10 @@ else {
 </div>
 
 <div>
-<div style="text-align: center;">
-<select id="method" name="method">
-        <option value="deposit">Deposit</option>
-        <option value="withdraw">Withdraw</option>
-</select>
+	<t1>Please Pick a Transaction Type:</t1><br>
+	<select id="method" name="method">
+		<option value="" disabled selected>Choose an Option</option>
+		<option value="deposit">Deposit</option>
+		<option value="withdraw">Withdraw</option>
+	</select>
 </div>
