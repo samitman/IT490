7f316ddb89cf68253e20cd12d3cb5925fbfeab62
scronin9031 <spc8@njit.edu>
2022-04-15 13:04:23 -0400

    Update db_creation.sql
    table creation works now

--- database/db_creation.sql
+++ database/db_creation.sql
@@ -1,19 +1,17 @@
 CREATE TABLE accounts (
-	'Uid' INT NOT NULL AUTO_INCREMENT
-	,'Email' VARCHAR(100) NOT NULL
-    ,'Username' VARCHAR(100) NOT NULL
-	,'Password' VARCHAR(100) NOT NULL
-    ,'FirstName' VARCHAR(60) NOT NULL
-    ,'LastName' VARCHAR(60) NOT NULL
-    ,'Balance' money(60) NOT NULL
-	,'eftMeme' float(60) NOT NULL
-    ,'eftBoomer' float(60) NOT NULL
-    ,'eftTech' float(60) NOT NULL
-    ,'eftCrypto' float(60) NOT NULL
-    ,'eftModerate' float(60) NOT NULL
-    ,'eftAggressive' float(60) NOT NULL
-    ,'eftGrowth' float(60) NOT NULL
-	,PRIMARY KEY ('id')
-    ENGINE=NDBCLUSTER
-	,
-	);
+	Uid INT NOT NULL AUTO_INCREMENT
+	,Email VARCHAR(100) NOT NULL
+    ,Username VARCHAR(100) NOT NULL
+	,Password VARCHAR(100) NOT NULL
+    ,FirstName VARCHAR(60) NOT NULL
+    ,LastName VARCHAR(60) NOT NULL
+    ,Balance float NOT NULL
+	,eftMeme float NOT NULL
+    ,eftBoomer float NOT NULL
+    ,eftTech float NOT NULL
+    ,eftCrypto float NOT NULL
+    ,eftModerate float NOT NULL
+    ,eftAggressive float NOT NULL
+    ,eftGrowth float NOT NULL
+	,PRIMARY KEY (Uid)
+	)ENGINE=NDBCLUSTER;
