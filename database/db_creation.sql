3d77fbc070f03106574445647a4c6bd453f2366a
scronin9031 <spc8@njit.edu>
2022-04-15 12:56:04 -0400

    Update db_creation.sql
    dbcreation update

--- database/db_creation.sql
+++ database/db_creation.sql
@@ -1,19 +1,19 @@
-CREATE TABLE `accounts` (
-	`Uid` INT NOT NULL AUTO_INCREMENT
-	,`Email` VARCHAR(100) NOT NULL
-    ,`Username` VARCHAR(100) NOT NULL
-	,`Password` VARCHAR(100) NOT NULL
-    ,`FirstName` VARCHAR(60) NOT NULL
-    ,`LastName` VARCHAR(60) NOT NULL
-    ,`Balance` money(60)
-	,'eftMeme' float(60)
-    ,'eftBoomer' float(60)
-    ,'eftTech' float(60)
-    ,'eftCrypto' float(60)
-    ,'eftModerate' float(60)
-    ,'eftAggressive' float(60)
-    ,'eftGrowth' float(60)
-	,PRIMARY KEY (`id`)
+CREATE TABLE accounts (
+	'Uid' INT NOT NULL AUTO_INCREMENT
+	,'Email' VARCHAR(100) NOT NULL
+    ,'Username' VARCHAR(100) NOT NULL
+	,'Password' VARCHAR(100) NOT NULL
+    ,'FirstName' VARCHAR(60) NOT NULL
+    ,'LastName' VARCHAR(60) NOT NULL
+    ,'Balance' money(60) NOT NULL
+	,'eftMeme' float(60) NOT NULL
+    ,'eftBoomer' float(60) NOT NULL
+    ,'eftTech' float(60) NOT NULL
+    ,'eftCrypto' float(60) NOT NULL
+    ,'eftModerate' float(60) NOT NULL
+    ,'eftAggressive' float(60) NOT NULL
+    ,'eftGrowth' float(60) NOT NULL
+	,PRIMARY KEY ('id')
     ENGINE=NDBCLUSTER
 	,
 	);
