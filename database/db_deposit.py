120e9cb3e33ce00ea5cb461f39bebf18f99989a4
scronin9031 <spc8@njit.edu>
2022-04-15 13:52:56 -0400

    DB_deposit
    deposit local test

--- database/db_deposit.py
+++ database/db_deposit.py
@@ -20,14 +20,18 @@ def dbinsertion(depositDict):
     msg = ''
     print(depositDict)
     cursor = mydb.cursor()
-    select_stmt=('SELECT * FROM accounts WHERE Username = %(Username)s AND Password = %(Password)s')
+    select_stmt=('SELECT * FROM accounts WHERE Username = %(Username)s')
     #UPDATE accounts set balance = balance + %(Deposit)s where Username = %(Username)
     cursor.execute(select_stmt, depositDict)
     account = cursor.fetchone()
     if account:
-        print("Account found, depositing cash!")
-        msg = '1'
-        return msg
+        print("Account found...")
+        insert_stmt=('UPDATE accounts SET Balance = Balance+%(Deposit)s WHERE Username = %(Username)s')
+    	cursor.execute(insert_stmt, depositDict)
+    	print("Deposit of: "+str(depositDict[1])+" made")
+    	mydb.commit()
+    	msg = '1'
+    	return msg
     else:
         print("Deposit failed")
         msg = '0'
