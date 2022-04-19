3c1f04f7abba10629055866d5c64dd99933520a7
scronin9031 <spc8@njit.edu>
2022-04-19 19:42:26 -0400

    login sending account details

--- database/login_localtest.py
+++ database/login_localtest.py
@@ -1,4 +1,3 @@
-from plistlib import UID
 import sys, os, mysql.connector
 
 def dbinsertion(credsdict):
@@ -14,22 +13,23 @@ def dbinsertion(credsdict):
     select_stmt=('SELECT * FROM accounts WHERE Username = %(Username)s AND Password = %(Password)s')
     cursor.execute(select_stmt, credsdict)
     account = cursor.fetchone()
-    print("This is what you get when you do fetchone:"+account)
+    print("This is what you get when you do fetchone:"+str(account))
     if account:
         print("Account found, logging in!")
-        uid = account[0]
-        email = account[1]
-        username = account[2]
-        first = account[4]
-        last = account[5]
-        balance = account[6]
-        eftMeme = account[7]
-        eftBoomer = account[8]
-        eftTech = account[9]
-        eftCrypto = account[10]
-        eftModerate = account[11]
-        eftAggressive = account[12]
-        eftGrowth = account[13]
+        uid = str(account[0])
+        email = str(account[1])
+        username = str(account[2])
+        first = str(account[4])
+        last = str(account[5])
+        balance = str(account[6])
+        eftMeme = str(account[7])
+        eftBoomer = str(account[8])
+        eftTech = str(account[9])
+        eftCrypto = str(account[10])
+        eftModerate = str(account[11])
+        eftAggressive = str(account[12])
+        eftGrowth = str(account[13])
+        
 
         msg = str(uid+","+email+","+username+","+first+","+last+","+balance+","+eftMeme+","+eftBoomer+","+eftTech+","+eftCrypto+","+eftModerate+","+eftAggressive+","+eftGrowth)
         return msg
