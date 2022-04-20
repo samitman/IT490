d97ea2915b6582bf3f4f099545efb8831fa89657
scronin9031 <spc8@njit.edu>
2022-04-20 16:38:22 -0400

    creating db invest

---
+++ database/db_invest.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import pika, sys, os, mysql.connector
+
+credentials = pika.PlainCredentials(username='test', password='test')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.192.60', credentials=credentials))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='deposit_be_db')
+ 
+
+def dbinsertion(investDict):
+    mydb = mysql.connector.connect(
+  host="localhost",
+  user="test",
+  password="1234",
+  database='test'
+)
+    msg = ''
+    print(investDict)
+    cursor = mydb.cursor()
+    select_stmt=('SELECT * FROM accounts WHERE Username = %(Username)s')
+    cursor.execute(select_stmt, investDict)
+    account = cursor.fetchone()
+    if account:
+        print("Account found...")
+        insert_stmt=('UPDATE accounts SET Balance = Balance+%(Deposit)s WHERE Username = %(Username)s')
+        cursor.execute(insert_stmt, investDict)
+        print("Balance adjusted")
+        mydb.commit()
+
+        insert_stmt=('UPDATE accounts SET Balance = Balance+%(Deposit)s WHERE Username = %(Username)s'
+
+        msg = '1'
+        return msg
+    else:
+        print("Deposit failed")
+        msg = '0'
+        return msg
+
+def on_request(ch, method, props, body):
+    print(" [x] Received %r" % body)
+    print(type(body))
+
+    messagestring = body.decode()
+    investList = messagestring.split(',')
+    username = investList[0]
+    etfName = investList[1]
+    depositAmount = investList[2]  
+    print("Split check:" + username +" "+etfName+" "+ depositAmount)
+    print(investList)
+    investDict =  {"Username": username, "etfName":etfName, "Deposit": depositAmount }
+
+    
+    response = dbinsertion(investDict)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(consumer_callback=on_request, queue='deposit_be_db')
+
+print(" [x] Awaiting RPC requests")
+print({on_request})
+channel.start_consuming()
