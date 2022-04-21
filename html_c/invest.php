7b44489ecaebf87d151fb5f27af10e71125a95fd
Sam Itman <si237@njit.edu>
2022-04-21 16:12:47 -0400

    front end select statement change

--- html_c/invest.php
+++ html_c/invest.php
@@ -51,8 +51,7 @@
             <option value="">Choose an Option</option>
             <option value="buy">Buy</option>
             <option value="sell">Sell</option>
-        
-            <br><br>
+        </select><br><br><br>
 
 		<input style="width: 25%; float: left;" type="number" id="investAmount" name="investAmount" placeholder="Amount to Invest" required/><br><br><br>
 
